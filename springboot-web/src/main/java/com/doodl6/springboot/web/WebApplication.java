fb3d622079cd4f5ba7fb125b0b5357f8aee2564f
MartinDai <xiaomingys@gmail.com>
2019-05-12 20:39:29 +0800

    redis配置调整

--- springboot-web/src/main/java/com/doodl6/springboot/web/WebApplication.java
+++ springboot-web/src/main/java/com/doodl6/springboot/web/WebApplication.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication(scanBasePackages = "com.doodl6")
 @MapperScan("com.doodl6.springboot.dao.api")
 @ServletComponentScan
+@EnableCaching
 public class WebApplication {
 
     public static void main(String[] args) {
--- springboot-web/src/main/java/com/doodl6/springboot/web/service/cache/redis/RedisConfig.java
+++ springboot-web/src/main/java/com/doodl6/springboot/web/service/cache/redis/RedisConfig.java
@@ -8,7 +8,6 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -22,50 +21,7 @@ import java.time.Duration;
  * Created by daixiaoming on 2018-12-05.
  */
 @Configuration
-@PropertySource("classpath:redis.properties")
 public class RedisConfig {
-
-    @Value("${hostName}")
-    private String hostName;
-
-    @Value("${port}")
-    private int port;
-
-    @Value("${pool.maxIdle}")
-    private int maxIdle;
-
-    @Value("${pool.minIdle}")
-    private int minIdle;
-
-    @Value("${pool.maxWaitMillis}")
-    private int maxWaitMillis;
-
-    @Value("${pool.maxTotal}")
-    private int maxTotal;
-
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(hostName);
-        redisStandaloneConfiguration.setPort(port);
-
-        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
-        poolConfig.setMaxIdle(maxIdle);
-        poolConfig.setMinIdle(minIdle);
-        poolConfig.setMaxWaitMillis(maxWaitMillis);
-        poolConfig.setMaxTotal(maxTotal);
-
-        LettucePoolingClientConfiguration lettucePoolingClientConfiguration = LettucePoolingClientConfiguration.builder()
-                .commandTimeout(Duration.ofSeconds(10))
-                .shutdownTimeout(Duration.ZERO)
-                .poolConfig(poolConfig)
-                .build();
-
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration, lettucePoolingClientConfiguration);
-        lettuceConnectionFactory.setShareNativeConnection(false);
-
-        return lettuceConnectionFactory;
-    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
