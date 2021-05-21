879f35e5d1769998fde7690c9004284d197dff6e
MartinDai <xiaomingys@gmail.com>
2021-05-21 23:21:44 +0800

    添加common-web的filter注解

--- springboot-web/src/main/java/com/doodl6/springboot/web/WebApplication.java
+++ springboot-web/src/main/java/com/doodl6/springboot/web/WebApplication.java
@@ -2,8 +2,10 @@ package com.doodl6.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.doodl6.springboot")
+@ServletComponentScan(basePackages = "com.doodl6.springboot.web.listener")
 public class WebApplication {
 
     public static void main(String[] args) {
