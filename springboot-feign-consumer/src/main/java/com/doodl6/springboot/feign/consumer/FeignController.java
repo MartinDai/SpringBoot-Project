ac2f9c6776d76d27d4d658f7433441cc05d0e80e
MartinDai <xiaomingys@gmail.com>
2022-07-11 16:32:04 +0800

    feign使用consul作为服务注册发现组件

--- springboot-feign-consumer/src/main/java/com/doodl6/springboot/feign/consumer/FeignController.java
+++ springboot-feign-consumer/src/main/java/com/doodl6/springboot/feign/consumer/FeignController.java
@@ -1,6 +1,9 @@
 package com.doodl6.springboot.feign.consumer;
 
 import com.doodl6.springboot.feign.consumer.service.FeignService;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +16,12 @@ import javax.annotation.Resource;
 public class FeignController {
 
     @Resource
+    private DiscoveryClient discoveryClient;
+
+    @Resource
+    private LoadBalancerClient loadBalancerClient;
+
+    @Resource
     private FeignService feignService;
 
     @RequestMapping(value = "/testEcho/{string}", method = RequestMethod.GET)
@@ -20,4 +29,19 @@ public class FeignController {
         return feignService.echo(string);
     }
 
+    /**
+     * 获取所有服务提供者节点信息
+     */
+    @GetMapping(value = "/getServices")
+    public Object getServices() {
+        return discoveryClient.getInstances("springboot-feign-provider");
+    }
+
+    /**
+     * 轮训获取服务提供者节点
+     */
+    @GetMapping(value = "/chooseService")
+    public Object chooseService() {
+        return loadBalancerClient.choose("springboot-feign-provider").getUri().toString();
+    }
 }
