ce6c3e3d95d0572df51b7b21a4a2b2b6b8d4dfe1
MartinDai <xiaomingys@gmail.com>
2025-01-25 14:42:02 +0800

    添加EventStream使用demo

---
+++ springboot-web/src/main/java/com/doodl6/springboot/web/controller/EventStreamController.java
@@ -0,0 +1,24 @@
+package com.doodl6.springboot.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+@Slf4j
+@RestController
+public class EventStreamController {
+
+    @GetMapping(value = "/eventStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> eventStream() {
+        // 创建一个 Flux，用于每秒推送一条消息
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> "服务器时间 " + LocalTime.now() + "\n\n")
+                .take(10)// 限制发送 10 条消息
+                .doOnCancel(() -> System.out.println("客户端连接已断开")); // 客户端断开时触发
+    }
+}
